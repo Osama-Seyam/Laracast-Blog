bf334b4a2fb291e057d9cbcc9d4e527e53f4025c
osama siam <osamamostafa065@gmail.com>
2024-08-06 01:36:31 +0300

    category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
@@ -17,6 +18,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Category::create([
+            "slug" => "personal",
+            "name" => "Personal",
+        ]);
+
         User::create([
             "name" => "osama seyam",
             "username" =>"osama",
